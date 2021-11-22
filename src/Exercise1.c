7a55e4a271fd4e4ebba03326a646ba01ed75f397
QuocBinh2708 <kamenrider27.sam@gmail.com>
2021-11-22 19:24:53 +0700

    ex1
    ititiu20169 lê kỳ quốc bình

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -12,7 +12,30 @@
 
 void Ex1(int n){
 	//Your codes here
-	
+	char *digit1[]= {"zero","one","two","three","four","five","six","seven","eight","nine"};
+    char *digit2[]= {"","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    char *digit3[]= {"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"}; 
+    while(n!=0){
+        if (n>=1000 && n<=9999 ){
+        printf("%s thousand", digit1[n/1000]);
+        n = n-(n/1000)*1000;}
+        else if  (n>=100 && n<=999){
+            printf(" %s hundred", digit1[n/100]);
+            n = n-(n/100)*100;
+        }
+        else if (n>=20 && n<=99){
+            printf(" %s ",digit3[n/10]);
+            n = n-(n/10)*10;
+        }
+        else if (n>=10 && n<=19){
+            printf(" %s ",digit2[n-10]);
+            n=0;
+        }
+        else if (n>=1 && n<=9){
+            printf("%s", digit1[n]);
+            n=0;
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
